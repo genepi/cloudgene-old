c309e90b483f940bbb46a7c0d84de7ef9cd1ac26
Sebastian Schoenherr <c703456@uibk.ac.at>
2012-07-23 16:31:30 +0200

    updating login user. edit logger

--- cloudgene.cluster/sql/create-user.sql
+++ cloudgene.cluster/sql/create-user.sql
@@ -1,1 +1,1 @@
-insert into USER VALUES (5,'cluster','a1234b3161b4fbfdfb96dd576b65bbea','','','','',true);
+insert into USER VALUES (5,'cloudgene','a1234b3161b4fbfdfb96dd576b65bbea','','','','',true);
