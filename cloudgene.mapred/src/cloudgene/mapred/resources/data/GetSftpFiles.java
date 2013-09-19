a8511683984f94d779e3fac7a5751482392b6d7b
Jonas Hagberg <jonas.hagberg@bils.se>
2013-09-19 11:26:36 +0200

    Created boilerplate code for sftp

---
+++ cloudgene.mapred/src/cloudgene/mapred/resources/data/GetSftpFiles.java
@@ -0,0 +1,63 @@
+package cloudgene.mapred.resources.data;
+
+import net.sf.json.JSONArray;
+
+import org.restlet.data.Form;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Post;
+import org.restlet.resource.ServerResource;
+
+import cloudgene.mapred.core.User;
+import cloudgene.mapred.core.UserSessions;
+import cloudgene.mapred.representations.LoginPageRepresentation;
+import cloudgene.mapred.util.FileItem;
+import cloudgene.mapred.util.Settings;
+
+public class GetSftpFiles extends ServerResource {
+
+	@Post
+	public Representation post(Representation entity) {
+
+		UserSessions sessions = UserSessions.getInstance();
+		User user = sessions.getUserByRequest(getRequest());
+
+		Form form = new Form(entity);
+		String node = form.getFirstValue("node");
+
+		StringRepresentation representation = null;
+
+		if (user != null) {
+
+			Settings settings = Settings.getInstance();
+			String workspace = settings.getLocalWorkspace(user.getUsername());
+
+			String rootNode = null;
+			if (node.equals("root")) {
+				rootNode = "";
+			} else {
+				rootNode = node;
+			}
+
+			FileItem[] items = cloudgene.mapred.util.SftpFileTree.getSftpFileTree(
+					workspace, rootNode);
+			JSONArray jsonArray = JSONArray.fromObject(items);
+
+			representation = new StringRepresentation(jsonArray.toString());
+			getResponse().setStatus(Status.SUCCESS_OK);
+			getResponse().setEntity(representation);
+			return representation;
+
+		} else {
+
+			representation = new LoginPageRepresentation();
+			getResponse().setStatus(Status.SUCCESS_OK);
+			getResponse().setEntity(representation);
+			return representation;
+
+		}
+
+	}
+
+}
---
+++ cloudgene.mapred/src/cloudgene/mapred/util/SftpFileTree.java
@@ -0,0 +1,83 @@
+package cloudgene.mapred.util;
+
+import java.util.Vector;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import net.sf.json.JSONArray;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+
+public class SftpFileTree {
+	
+	public static FileItem[] getSftpFileTree(String path) {
+		String SFTPHOST = "localhost";
+		int    SFTPPORT = 49165;
+		String SFTPUSER = "root";
+		String SFTPPASS = "Deg";
+		//String SFTPWORKINGDIR = "/root";
+				
+		Session 	session 	= null;
+		Channel 	channel 	= null;
+		ChannelSftp channelSftp = null;
+		try{
+			JSch jsch = new JSch();
+			session = jsch.getSession(SFTPUSER,SFTPHOST,SFTPPORT);
+			session.setPassword(SFTPPASS);
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			channel = session.openChannel("sftp");
+			channel.connect();
+			channelSftp = (ChannelSftp)channel;
+			channelSftp.cd(path);
+			Vector<ChannelSftp.LsEntry> filelist = channelSftp.ls(path);
+			FileItem[] results = null;
+			results = new FileItem[filelist.size()];
+			int count = 0;
+			
+			for(ChannelSftp.LsEntry entry : filelist) {
+			 if(entry.getAttrs().isDir()){
+				// System.out.println(entry.getLongname());
+				 results[count] = new FileItem();
+				 results[count].setText(entry.getFilename());
+				 results[count].setPath(path + "/" + entry.getFilename());
+				 results[count].setCls("folder");
+				 count++;
+			 }
+			}
+			// System.out.println(count);
+				//System.out.println(entry.getFilename());
+				//System.out.println(entry.getAttrs());
+				//System.out.println(entry.getLongname());
+				//System.out.println(entry.getAttrs().isDir());
+			 for(ChannelSftp.LsEntry entry : filelist) {
+				 if(!entry.getAttrs().isDir()){
+					results[count] = new FileItem();
+					results[count].setText(entry.getFilename());
+					results[count].setPath(path + "/" + entry.getFilename());
+					results[count].setId(path + "/" + entry.getFilename());
+					results[count].setLeaf(true);
+					results[count].setCls("file");
+					results[count].setSize(FileUtils
+							.byteCountToDisplaySize(entry.getAttrs().getSize()));
+					count++;
+				 }
+			}
+			return results;
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+			return null;
+		}
+		
+		
+	}
+
+}
