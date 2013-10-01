23bb3dcd540f01f0008601460deb573069f88ca1
Jonas Hagberg <jonas.hagberg@bils.se>
2013-10-01 10:07:18 +0200

    Big refactor of SftpFileTree. Should now handle broken links in a better fashion.

--- cloudgene.mapred/src/cloudgene/mapred/util/SftpFileTree.java
+++ cloudgene.mapred/src/cloudgene/mapred/util/SftpFileTree.java
@@ -2,12 +2,12 @@ package cloudgene.mapred.util;
 
 import java.util.Vector;
 
-import cloudgene.mapred.resources.data.GetSftpFiles;
-
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -20,61 +20,136 @@ public class SftpFileTree {
 		Session 	session 	= null;
 		Channel 	channel 	= null;
 		ChannelSftp channelSftp = null;
-		try{
+		
 			JSch jsch = new JSch();
-			session = jsch.getSession(SFTPUSER,SFTPHOST,SFTPPORT);
+			try {
+				session = jsch.getSession(SFTPUSER,SFTPHOST,SFTPPORT);
+			} catch (JSchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
 			session.setPassword(SFTPPASS);
 			java.util.Properties config = new java.util.Properties();
 			config.put("StrictHostKeyChecking", "no");
 			session.setConfig(config);
-			session.connect();
-			channel = session.openChannel("sftp");
-			channel.connect();
+			try {
+				session.connect();
+			} catch (JSchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			try {
+				channel = session.openChannel("sftp");
+			} catch (JSchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			try {
+				channel.connect();
+			} catch (JSchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
 			channelSftp = (ChannelSftp)channel;
 			log.info("PATH IS " + path);
-			if(path.equals("~/")) {path = channelSftp.pwd();}
+			if(path.equals("~/")) {try {
+				path = channelSftp.pwd();
+			} catch (SftpException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}}
 			log.info("PATH IS  after if " + path);
-			log.info("pwd is  " + channelSftp.pwd());
-			channelSftp.cd(path);
-			Vector<ChannelSftp.LsEntry> filelist = channelSftp.ls(path);
-			log.info("pwd is  " + channelSftp.pwd());
+			
+			try {
+				channelSftp.cd(path);
+			} catch (SftpException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+			Vector<ChannelSftp.LsEntry> filelist = null;
+			try {
+				filelist = channelSftp.ls(path);
+			} catch (SftpException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		//	log.info("pwd is  " + channelSftp.pwd());
 			FileItem[] results = null;
 			//-2 to take away folder ".." and "."
 			results = new FileItem[filelist.size()-2];
 			int count = 0;
-			
-			for(ChannelSftp.LsEntry entry : filelist) {
-			 if(((ChannelSftp.LsEntry) channelSftp.ls(channelSftp.realpath(entry.getFilename())).get(0)).getAttrs().isDir()  && !((entry.getFilename().equals(".") || (entry.getFilename().equals(".."))) ) ){
-				 results[count] = new FileItem();
-				 results[count].setId(path + "/" + entry.getFilename());
-				 results[count].setText(entry.getFilename());
-				 results[count].setPath(channelSftp.realpath(entry.getFilename()));
-				 results[count].setCls("x-dd-drop-nodrop");
-				 results[count].setDisabled(true);
-				 results[count].setLeaf(false); 
-				 count++;
-			 }
-			}
-			
-			 for(ChannelSftp.LsEntry entry : filelist) {
-				 if(!((ChannelSftp.LsEntry) channelSftp.ls(channelSftp.realpath(entry.getFilename())).get(0)).getAttrs().isDir()){
+			for (ChannelSftp.LsEntry entry : filelist) {
+
+				if (entry.getAttrs().isLink()) {
+					String link = null;
+					boolean linkIsdir = false;
+					try {
+						link = channelSftp.readlink(entry.getFilename());
+					} catch (SftpException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						linkIsdir = ((ChannelSftp.LsEntry) channelSftp.ls(link)
+								.get(0)).getAttrs().isDir();
+					} catch (com.jcraft.jsch.SftpException ex) {
+						if (ex.getMessage().equals("No such file")) {
+							results[count] = new FileItem();
+							results[count].setText(entry.getFilename()
+									+ " BROKEN LINK");
+							results[count].setLeaf(true);
+							results[count].setCls("file");
+							results[count].setDisabled(true);
+							count++;
+						} else {
+							ex.printStackTrace();
+						}
+					}
+					if (linkIsdir) {
+						results[count] = new FileItem();
+						results[count].setText(entry.getFilename());
+						results[count].setLeaf(false);
+						results[count].setCls("folder");
+						results[count].setId(path + "/" + entry.getFilename());
+						results[count].setPath(path + "/" + entry.getFilename());
+						count++;
+					}
+				} else if (entry.getAttrs().isDir()
+						&& !((entry.getFilename().equals(".") || (entry
+								.getFilename().equals(".."))))) {
 					results[count] = new FileItem();
 					results[count].setText(entry.getFilename());
-					results[count].setPath(channelSftp.realpath(entry.getFilename()));
+					results[count].setLeaf(false);
+					results[count].setCls("folder");
+					results[count].setId(path + "/" + entry.getFilename());
+					results[count].setPath(path + "/" + entry.getFilename());
+					count++;
+				} else {
+					results[count] = new FileItem();
+					results[count].setText(entry.getFilename());
+					results[count].setPath(path +"/" + entry.getFilename());
 					results[count].setId(path +"/" + entry.getFilename());
 					results[count].setLeaf(true);
 					results[count].setCls("file");
 					results[count].setSize(FileUtils
 							.byteCountToDisplaySize(entry.getAttrs().getSize()));
 					count++;
-				 }
+				}
+					
 			}
+			
+			
+			
 			return results;
 			
-		}catch(Exception ex){
-			ex.printStackTrace();
-			return null;
-		}
+		
 		
 		
 	}
